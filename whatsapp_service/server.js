43219ce0618c9817c540df6e2ed1591e04f13e43
Nycksion Cavalcante <nycksion.ia@gmail.com>
2026-03-04 20:51:09 -0300

    Ajuste final do zap

--- whatsapp_service/server.js
+++ whatsapp_service/server.js
@@ -6,7 +6,21 @@ const app = express();
 app.use(express.json());
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ],
+        executablePath: '/usr/bin/google-chrome'
+    }
 });
 
 client.on('qr', (qr) => qrcode.generate(qr, { small: true }));
